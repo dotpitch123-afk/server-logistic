0ae2fd159650aca80e854bdda2ffd44d0d86e928
manishkuma123 <manishp.dotpitchtechnologies@gmail.com>
2026-05-26 11:50:58 +0530

    code upate

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/test', (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
-app.listen(process.env.PORT || 3000)
 
